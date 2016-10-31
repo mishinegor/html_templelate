41fbf6f121f9f4979fcc1fbba1e943331542e953
mishinegor <me@mishinegor.me>
2016-10-31 19:23:56 +0300

    Concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
-var gulp = require('gulp');
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-cofee');
 
-gulp.task('log', function() {
-    gutil.log('HTML for ipp');
+var jsSources =[
+    'components/script/file.js',
+    'components/script/file.js',
+    'components/script/file.js',
+    'components/script/file.js'
+];
+
+gulp.task('coffee', function() {
+    gulp.src('');
+    
+});
+
+gulp.task('js', function() {
+    gulp.src(jsSources);
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
     
 });
